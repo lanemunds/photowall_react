3665c73972192148b7fc3eea1d68b41206a57d9c
lanemunds <lanemunds@gmail.com>
2022-12-29 10:34:33 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM  from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+const element = React.createElement('ol',null, 
+React.createElement('li',null,'take out the trash'),
+React.createElement('li',null,'shovel the driveway'),
+React.createElement('li',null,'walk the dog')
+);
+ReactDOM.render(element,document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
