9a868d6dc43721af8c7da83e0401497eba94433b
lanemunds <lanemunds@gmail.com>
2023-01-06 14:20:12 -0700

    brower router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import  ReactDOM  from 'react-dom';
 import Main from './component/Main'
 import './styles/stylesheets.css'
+import { BrowserRouter } from 'react-router-dom';
 
 
 
 
-ReactDOM.render(<Main/>,document.getElementById('root')
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>,document.getElementById('root')
 );
 
 
