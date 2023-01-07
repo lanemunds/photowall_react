7ba1eed3de87a3d95281685a2b40e1d0131711de
lanemunds <lanemunds@gmail.com>
2023-01-07 09:55:45 -0700

    link

--- src/component/photoWall.js
+++ src/component/photoWall.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Photo from './photo'
 import propTypes from 'prop-types'
+import { Link } from "react-router-dom";
 
 function PhotoWall(props){
     return <div>
-        <a  className="addIcon" onClick={props.onNavigate} href = "#AddPhoto">+</a>
+    <Link className="addIcon"  to = "/AddPhoto">+</Link>
     <div className="photoGrid">
     {props.posts.map((post, index) => <Photo key = {index} post = {post} onRemovePhoto = {props.onRemovePhoto} />)}
 </div>
