aae3fd51c63efd058434042f7eda329adc8143a9
lanemunds <lanemunds@gmail.com>
2023-01-11 12:59:43 -0700

    add form

--- src/component/AddPhoto.js
+++ src/component/AddPhoto.js
@@ -2,8 +2,18 @@ import React, {Component} from 'react'
 
 class AddPhoto extends Component{
     render(){
-        return <h1>This is the page where we will add photos</h1>
-    }
-}
+        return(
+            
+            <div>
+            <h1>PhotoWall</h1>
+            <div className='form'>
+            <form>
+                <input type="text" placeholder='Link'/>
+                <input type="text" placeholder='Description'/>
+                <button>post</button>
+            </form>
+            </div>
+            </div>
+    )}}
 
 export default AddPhoto
--- src/component/photoWall.js
+++ src/component/photoWall.js
@@ -5,7 +5,9 @@ import { Link } from "react-router-dom";
 
 function PhotoWall(props){
     return <div>
-    <Link className="addIcon"  to = "/AddPhoto">+</Link>
+    <Link className="addIcon"  to = "/AddPhoto">
+        +
+    </Link>
     <div className="photoGrid">
     {props.posts.map((post, index) => <Photo key = {index} post = {post} onRemovePhoto = {props.onRemovePhoto} />)}
 </div>
